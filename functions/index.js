cd1f98a4749de83ec2c56c5b5087572a7dd6da48
keyadd <keyfromadd@gmail.com>
2024-08-15 12:23:26 +0800

    修正

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 
-export const onRequest = async({require})=>{
-    return await new Response("Hello, world!")
+export const onRequestGet = async({require})=>{
+
+   // return await new Response("Hello, world!")
+    return new Response("Hello, world!", {status: 200})
+   // return await new Response("Hello, world!")
 }
