e11f88f2c140f2e5c1eabafe5f377314f3e04c9d
keyadd <keyfromadd@gmail.com>
2024-08-15 15:28:22 +0800

    test添加log24

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,12 @@
 const addr = 'worker-us-vless.keyfromadd.workers.dev';
 
-export async function onRequest(context) {
-    const request = context.request;
-    const url = new URL(request.url);
-
-    // 打印原始请求的 URL 和查询参数
-    console.log(`Original URL: ${url.href}`);
-    console.log(`Query Parameters: ${url.searchParams.toString()}`);
-    console.log(`Original Pathname: ${url.pathname}`);
-
-    // 检查并处理路径
+export default {
+  async fetch(request, env) {
+    let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-        // 修改请求的目标主机名
-        url.hostname = addr;
-
-        // 构建新的请求对象
-        const newRequest = new Request(url.toString(), {
-            method: request.method,
-            headers: request.headers,
-            body: request.method === 'POST' || request.method === 'PUT' ? request.body : null, // 仅在 POST 或 PUT 请求中有 body
-            redirect: request.redirect,
-        });
-
-        console.log(`Forwarded URL: ${newRequest.url}`);
-
-        try {
-            // 转发请求到目标服务器
-            return await fetch(newRequest);
-        } catch (error) {
-            console.error("Error during request forwarding:", error);
-            return new Response("Error during request forwarding.", { status: 502 });
-        }
+      url.hostname = addr
+      let new_request = new Request(url, request);
+      return fetch(new_request);
     }
-
-    // 如果路径不符合条件，返回原始请求
-    return await fetch(request);
-}
+    return env.ASSETS.fetch(request);
+  },};
