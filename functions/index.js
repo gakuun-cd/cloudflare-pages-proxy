8612e8ff6b3f8e639796e00c451f103731c111f6
keyadd <keyfromadd@gmail.com>
2024-08-15 12:30:23 +0800

    test反向代理

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,14 @@
-export function onRequest(context) {
-	return new Response("Hello, world!")
+export function onRequest(request) {
+
+    const url = new URL(request.url);
+
+    // 指定你要代理的目标服务器
+    url.hostname = "blog.acorn.eu.org";
+
+    // 可以根据需要修改请求头或添加其他逻辑
+    // request.headers.set('X-Forwarded-For', 'your-custom-header');
+
+    // 转发请求到目标服务器并返回响应
+    return  fetch(url.toString(), request);
   }
+
