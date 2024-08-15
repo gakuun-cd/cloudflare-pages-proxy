cac84e235c3733e885a361d61cb713132b87e754
keyadd <keyfromadd@gmail.com>
2024-08-15 13:45:35 +0800

    test添加log10

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,8 @@ export async function onRequest(context) {
     
 
     //const url = new URL(context.url);
-    console.log(`${context.request}`);
+    console.log(context.request);
+    print(context);
     
     return new Response("hello", {status: 404})
     // 指定你要代理的目标服务器
