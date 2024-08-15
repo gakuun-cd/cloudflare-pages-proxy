be536ac7c3e6e3ad68e3e7c2b8a3f9fd7d52d8be
keyadd <keyfromadd@gmail.com>
2024-08-15 14:29:14 +0800

    test添加log18

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,24 @@
-export async function onRequest(context) {
-    
+/**
+ * 反向代理 代理http  websocket
+ * @param {} context 
+ * @returns 
+ */
 
+const addr = 'worker-us-vless.keyfromadd.workers.dev'
+
+export async function onRequest(context,e) {
     const url = new URL(context.request.url);
-    
-    //print(context);
-    
-    //return new Response("hello", {status: 404})
-    // 指定你要代理的目标服务器
-    url.hostname = "worker-us-vless.keyfromadd.workers.dev";
-    console.log(`[LOGGING FROM /hello]: Request came from ${url.hostname}`);
 
-    // 可以根据需要修改请求头或添加其他逻辑
-    // request.headers.set('X-Forwarded-For', 'your-custom-header');
+
+    // 指定你要代理的目标服务器
+
+    if(url.pathname.startsWith('/')){
+      url.hostname = addr;
+      let new_request = new Request(url,context.request)
+      return await fetch(new_request);
+    }
+    
+    console.log(`[LOGGING FROM /hello]: Request came from ${url.hostname}`);
 
     // 转发请求到目标服务器并返回响应
     return await fetch(url.toString(), context.request);
