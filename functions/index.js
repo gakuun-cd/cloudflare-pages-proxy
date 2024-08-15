609be8ec28cd8f436a15273a87d47c52c7721a3d
keyadd <keyfromadd@gmail.com>
2024-08-15 13:56:48 +0800

    test添加log15

--- functions/index.js
+++ functions/index.js
@@ -2,16 +2,17 @@ export async function onRequest(context) {
     
 
     const url = new URL(context.request.url);
-    console.log(`[LOGGING FROM /hello]: Request came from ${url.hostname}`);
+    
     //print(context);
     
-    return new Response("hello", {status: 404})
+    //return new Response("hello", {status: 404})
     // 指定你要代理的目标服务器
     url.hostname = "blog.acorn.eu.org";
+    console.log(`[LOGGING FROM /hello]: Request came from ${url.hostname}`);
 
     // 可以根据需要修改请求头或添加其他逻辑
     // request.headers.set('X-Forwarded-For', 'your-custom-header');
 
     // 转发请求到目标服务器并返回响应
-    //return await fetch(url.toString(), request);
+    return await fetch(url.toString(), context);
   }
